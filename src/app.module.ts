2741141abf1d784a5a037ed6e29138d9c434931c
WillianM19 <willianm1928@gmail.com>
2024-03-21 10:14:25 -0300

    🗃️ Configurando banco de dados e model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { CommentModule } from './comments/comments.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommentModel } from './comments/comments.model';
 
+
+const databaseConfig: TypeOrmModule = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'root',
+  entities: [CommentModel],
+  database: 'testenest',
+  synchronize: true,
+  logging: true,
+}
 
 @Module({
-  imports: [CommentModule],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    CommentModule,
+  ],
   controllers: [],
   providers: [],
 })
