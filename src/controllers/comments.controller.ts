a6a05aae1be55f5e3843fae1e8cd36a6bfd337a6
WillianM19 <willianm1928@gmail.com>
2024-03-21 11:57:38 -0300

    ✨ Base de votos

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -1,59 +1,96 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, ValidationPipe } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { CommentsModel } from "src/models/comments.model";
-import { CommentsSchema } from "src/schemas/comments.schema";
-import { Repository, WithoutId } from "typeorm";
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  ValidationPipe,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentsModel } from 'src/models/comments.model';
+import { CommentsSchema } from 'src/schemas/comments.schema';
+import { Repository, WithoutId } from 'typeorm';
 
 @Controller('/comments')
 export class CommentsController {
-    constructor(@InjectRepository(CommentsModel) private model: Repository<CommentsModel>){};
+  constructor(
+    @InjectRepository(CommentsModel) private model: Repository<CommentsModel>,
+  ) {}
 
-    //CRUD
-    @Get()
-    async getAll() {
-        //Apenas comentários ativos
-        const list = await this.model.findBy({ active: true })
-        return list;
+  //CRUD
+  @Get()
+  async getAll() {
+    //Apenas comentários ativos
+    const list = await this.model.findBy({ active: true });
+    return list;
+  }
+
+  @Post()
+  async postComment(@Body() body: CommentsSchema) {
+    const newComment: Omit<CommentsModel, 'comment_id'> = {
+      ...body,
+      active: true,
+      created_at: new Date(),
+      vote_points: 0,
+    };
+
+    await this.model.save(newComment);
+    return { message: 'Comentário registrado!', comment: newComment };
+  }
+  @Put(':id')
+  async editComment(@Param('id') id: number, @Body() body: CommentsSchema) {
+    const editingComment = await this.model.findOneBy({ comment_id: id });
+    const editedComment: Omit<CommentsModel, 'comment_id'> = {
+      ...editingComment,
+      ...body,
+    };
+
+    await this.model.update({ comment_id: id }, editedComment);
+
+    return { message: 'Comentário editado!', comment: editedComment };
+  }
+
+  @Delete(':id')
+  async deleteComment(@Param('id') id: number) {
+    const comment = await this.model.findOneBy({ comment_id: id });
+    await this.model.update({ comment_id: id }, { ...comment, active: false });
+
+    return {
+      message: 'Comentário removido (softdelete)',
+      comment: { ...comment, active: false },
+    };
+  }
+
+  @Delete('harddelete/:id')
+  async hardDeleteComment(@Param('id') id: number) {
+    await this.model.delete({ comment_id: id });
+
+    return { message: 'Comentário removido (harddelete)' };
+  }
+
+  //Votes
+  async handleVote(type: 'add' | 'remove', id: number) {
+    const voteComment = await this.model.findOneBy({ comment_id: id });
+    await this.model.update(
+        { comment_id: id },
+        { ...voteComment, vote_points: type == 'add' ? voteComment.vote_points + 1 :  voteComment.vote_points - 1},
+    );
+    return {
+        message: `${type == 'add' ? 'upvote' : 'downvote'} adicionado com sucesso!`
     }
-    
-    @Post()
-    async postComment(@Body() body: CommentsSchema) {
-        const newComment: Omit<CommentsModel, 'comment_id'>  = {
-            ...body,
-            active: true,
-            created_at: new Date(),
-            vote_points: 0,
-        }
+  }
 
-        await this.model.save(newComment)
-        return {message: 'Comentário registrado!', comment: newComment};
-    }
-    @Put(':id')
-    async editComment(@Param('id') id: number, @Body() body: CommentsSchema) {
-        const editingComment = await this.model.findOneBy({ comment_id: id })
-        const editedComment: Omit<CommentsModel, 'comment_id'> = {
-            ...editingComment,
-            ...body,
-        }
 
-        await this.model.update({comment_id: id}, editedComment)
-
-        return {message: 'Comentário editado!', comment: editedComment};
-    }
-
-    @Delete(':id')
-    async deleteComment(@Param('id') id: number) {
-        const comment = await this.model.findOneBy({comment_id: id})
-        await this.model.update({comment_id: id}, {...comment, active: false})
-
-        return {message: 'Comentário removido (softdelete)', comment: {...comment, active: false}}
-    }
-
-    @Delete('harddelete/:id')
-    async hardDeleteComment(@Param('id') id: number) {
-        await this.model.delete({comment_id: id})
-
-        return {message: 'Comentário removido (harddelete)'}
-    }
+  @Patch('add-upvote/:id')
+  async addUpVote(@Param('id') id: number) {
+    return this.handleVote('add', id)
+  }
+  
+  @Patch('add-downvote/:id')
+  async addDownVote(@Param('id') id: number) {
+    return this.handleVote('remove', id)
+  }
 }
